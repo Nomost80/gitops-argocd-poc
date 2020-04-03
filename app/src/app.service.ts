88ab9275a8229264274b0f3605f3861be5155e85
Nomost <guillaume.soufflet1@viacesi.fr>
2020-04-04 00:59:58 +0200

    fix: chart repo target

--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -5,6 +5,6 @@ const { KUBERNETES_NAMESPACE } = process.env;
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Hello World in ${KUBERNETES_NAMESPACE}!!!!!!!!!!!!!`;
+    return `Hello World in ${KUBERNETES_NAMESPACE}!!!`;
   }
 }
